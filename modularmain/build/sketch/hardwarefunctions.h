a6fcf0efc74fd258664bc0d97e490a2021c2e0a8
henri-32 <henrikirchhoff@gmail.com>
2026-01-15 11:48:05 +0000

    a lot of small fixes

---
+++ modularmain/build/sketch/hardwarefunctions.h
@@ -0,0 +1,16 @@
+#line 1 "/home/henri-32/Softwareprojekte/Arduinoprojekte/Heizungssteuerung_git/modularmain/hardwarefunctions.h"
+// hardwarefunctions.h
+#pragma once
+#include "variables.h"
+#include "logicfunctions.h"
+#include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+void relaisstarten(RELAISZUSTAND zielzustand, unsigned long now);
+void relaischeck_loesen(unsigned long now);
+void anaus_Schalter(unsigned long now);
+void mode_Schalter(unsigned long now);
+void display_switch(unsigned long now);
+void startTemperatureRequest(unsigned long now);
+void readTemperature(unsigned long now);
---
+++ modularmain/build/sketch/main.h
@@ -0,0 +1,5 @@
+#line 1 "/home/henri-32/Softwareprojekte/Arduinoprojekte/Heizungssteuerung_git/modularmain/main.h"
+#pragma once
+
+void setup();
+void loop();
